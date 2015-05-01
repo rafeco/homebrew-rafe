de2df247179ce62d0503578c3c3cd0b792cd7a7a
Rafe Colburn <rcolburn@etsy.com>
2015-05-01 13:05:31 -0400

    Upgraded to version 7.1.1

--- vertica.rb
+++ vertica.rb
@@ -1,13 +1,12 @@
-require "formula"
-
-# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
 #                /usr/local/Library/Contributions/example-formula.rb
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Vertica < Formula
   homepage "http://my.vertica.com/"
-  url "https://dl.dropboxusercontent.com/u/5514085/vertica-client-6.1.0-0.mac.tar.gz"
-  sha1 "32a06c0b38e531f464d6e3925f07cbc4944bf7b4"
+  url "https://dl.dropboxusercontent.com/u/5514085/vertica-client-7.1.1-0.mac.64.tar.gz"
+  version "7.1.1"
+  sha256 "0d2982f7e65c72f54572bf0f15fc25937384eb6588a8097406e00e796d0a6e58"
 
   # depends_on "cmake" => :build
   # depends_on :x11 # if your formula requires any X11/XQuartz components
